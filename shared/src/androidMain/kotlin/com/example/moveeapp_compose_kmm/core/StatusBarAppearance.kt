55036e1608857d08faa00a7405e179dda2a901f8
M. Utku Ensert <mutkuensert@hotmail.com>
2023-10-06 10:21:33 +0300

    Update remember use in StatusBarAppearance

--- shared/src/androidMain/kotlin/com/example/moveeapp_compose_kmm/core/StatusBarAppearance.kt
+++ shared/src/androidMain/kotlin/com/example/moveeapp_compose_kmm/core/StatusBarAppearance.kt
@@ -2,15 +2,13 @@ package com.example.moveeapp_compose_kmm.core
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 
 @Composable
 actual fun StatusBarAppearance(isBackgroundLight: Boolean) {
     val insetsController = findWindow()?.getInsetsControllerCompat()
-    val isDefaultAppearanceLight by remember {
-        mutableStateOf(insetsController?.isAppearanceLightStatusBars)
+    val isDefaultAppearanceLight = remember(key1 = Unit) {
+        insetsController?.isAppearanceLightStatusBars
     }
 
     DisposableEffect(isBackgroundLight) {
