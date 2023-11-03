67c33bea2c82ae43072de59d23a99c2e58164112
Sena Aydın <senanuraydin@gmail.com>
2023-11-03 19:05:04 +0300

    Update ios map view

--- shared/src/iosMain/kotlin/com/example/moveeapp_compose_kmm/map/Map.kt
+++ shared/src/iosMain/kotlin/com/example/moveeapp_compose_kmm/map/Map.kt
@@ -5,13 +5,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.interop.UIKitView
+import com.example.moveeapp_compose_kmm.MR
+import com.example.moveeapp_compose_kmm.domain.location.DeviceLocation
 import com.example.moveeapp_compose_kmm.ui.scene.map.Cinema
 import com.example.moveeapp_compose_kmm.ui.scene.map.MapUiState
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
 import platform.CoreLocation.CLLocationCoordinate2DMake
-import platform.MapKit.MKMapCamera
+import platform.MapKit.MKAnnotationView
+import platform.MapKit.MKCoordinateRegionMakeWithDistance
 import platform.MapKit.MKMapView
 import platform.MapKit.MKMapViewDelegateProtocol
+import platform.MapKit.MKPointAnnotation
 import platform.darwin.NSObject
 
 @OptIn(ExperimentalForeignApi::class)
@@ -19,26 +24,95 @@ import platform.darwin.NSObject
 actual fun Map(
     modifier: Modifier,
     uiState: MapUiState,
-    onMarkerClick: (Cinema?) -> Unit
+    onMarkerClick: (Cinema?) -> Unit,
+    onPositionChange: (DeviceLocation) -> Unit
 ) {
-    val mkMapView = remember { MKMapView().apply {
-        setUserInteractionEnabled(true)
-    } }
 
-    val mkMapCamera = remember { MKMapCamera().apply {
-        setCenterCoordinate(CLLocationCoordinate2DMake(18.982579225106615, -99.09380710785197))
-        setAltitude(50000.0)
-    } }
+    val location = CLLocationCoordinate2DMake(
+        uiState.lastLocation?.latitude ?: 0.0,
+        uiState.lastLocation?.longitude ?: 0.0
+    )
+    val annotation = remember {
+        MKPointAnnotation(
+            location,
+            title = null,
+            subtitle = null
+        )
+    }
+
+    val mkMapView = remember {
+        MKMapView().apply {
+            addAnnotation(annotation)
+            setUserInteractionEnabled(true)
+            showsUserLocation = true
+            pitchEnabled = true
+            scrollEnabled = true
+            zoomEnabled = true
+        }
+    }
+
+    val delegate = remember {
+        MKDelegate { annotation ->
+            if (annotation != null) {
+                val deviceLocation = annotation.coordinate.useContents {
+                    DeviceLocation(latitude, longitude)
+                }
+                onMarkerClick.invoke(
+                    Cinema(
+                        annotation.title ?: "",
+                        annotation.subtitle ?: "",
+                        deviceLocation
+                    )
+                )
+            }
+        }
+    }
 
     UIKitView(
         modifier = modifier.fillMaxSize(),
         interactive = true,
         factory = {
-            mkMapView.delegate = object : NSObject(), MKMapViewDelegateProtocol{
-
-            }
             mkMapView
+        }, update = { view ->
+            mkMapView.setRegion(
+                MKCoordinateRegionMakeWithDistance(
+                    centerCoordinate = location,
+                    10_000.0, 10_000.0
+                ),
+                animated = true
+            )
+            mkMapView.setDelegate(delegate)
+
+            val pins = uiState.cinemaList.map { item ->
+                val pin = MKPointAnnotation()
+                val annotationView = mkMapView.dequeueReusableAnnotationViewWithIdentifier("custom")
+                    ?: MKAnnotationView(pin, "custom")
+
+                annotationView.setAnnotation(pin)
+                annotationView.setImage(MR.images.ic_heart_filled.toUIImage())
+                val coordinates = item.location
+
+                pin.setCoordinate(
+                    CLLocationCoordinate2DMake(
+                        coordinates.latitude ?: 0.0,
+                        coordinates.longitude ?: 0.0
+                    )
+                )
+
+                pin.setTitle(item.name)
+                pin.setSubtitle(item.description)
+                pin
+            }
+            mkMapView.addAnnotations(pins)
         }
     )
+}
 
+private class MKDelegate(
+    private val onAnnotationClicked: (MKPointAnnotation?) -> Unit
+) : NSObject(), MKMapViewDelegateProtocol {
+    override fun mapView(mapView: MKMapView, didSelectAnnotationView: MKAnnotationView) {
+        val annotation = didSelectAnnotationView.annotation as MKPointAnnotation
+        onAnnotationClicked(annotation)
+    }
 }
